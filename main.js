7aeeae01751c5e269d38d157a92eada001b48aeb
Awaarus <Tom.Sid@wp.pl>
2019-09-03 14:34:43 +0000

    anime.js kinda cool, animation done

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 anime({
-    targets: '.box',
-    translateX:250
+    targets: '.Top',
+    //translateY: 166,
+    keyframes: [
+        {translateX: 83, translateY: 90, rotate: 90, background: '#0000ff'},
+        {translateX: 0, translateY:166, rotate: 180, background: '#000000'},
+        {translateX: -83, translateY: 90, rotate: 270, background: '#ff0000'},
+        {translateX: 0, translateY: 0, rotate:360, background: '#000000'}
+    ],
+    duration:6500,
+    loop: true
 });
+
+anime({
+    targets: '.Bottom',
+    keyframes: [
+        {translateX: -83, translateY: -90, rotate: 90, background: '#ff0000'},
+        {translateX: 0, translateY: -180, rotate: 180, background: '#ffffff'},
+        {translateX: 83, translateY: -90, rotate: 270, background: '#0000ff'},
+        {translateX: 0, translateY: 0, rotate: 360, background: '#ffffff'}
+    ],
+    duration:6500,
+    loop: true
+});
+
+anime({
+    targets:'.Logo',
+    keyframes:[
+        {borderRadius: '50%'},
+        {borderRadius: '0%'}
+    ],
+    duration:3250,
+    loop: true
+})
