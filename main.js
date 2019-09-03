1b510533c97b11de1f10a11a478ebdf165e49ae9
Awaarus <Tom.Sid@wp.pl>
2019-09-03 14:58:15 +0000

    working on animating svg using anime.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 anime({
     targets: '.Top',
-    //translateY: 166,
+    //keyframe segir til hvað á að gerast og í hvaða röð það á að gerast
     keyframes: [
         {translateX: 83, translateY: 90, rotate: 90, background: '#0000ff'},
         {translateX: 0, translateY:166, rotate: 180, background: '#000000'},
         {translateX: -83, translateY: 90, rotate: 270, background: '#ff0000'},
         {translateX: 0, translateY: 0, rotate:360, background: '#000000'}
     ],
+    //duration segir hversu lengi það á að taka fyrir ÖLL keyframe að spila
     duration:6500,
+    //þegar það endar byrjar það upp á nýtt
     loop: true
 });
 
@@ -32,3 +34,4 @@ anime({
     duration:3250,
     loop: true
 })
+
