bfd71799eb64d0b2724de4ade5da0cad5f5b14b2
Awaarus <Tom.Sid@wp.pl>
2019-09-25 15:43:04 +0000

    finished writing shaders, and draw function

--- main.js
+++ main.js
@@ -48,6 +48,10 @@ function main(){
             modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
         },
     };
+    //hér er rútínan kölluð á yfir því sem ég er að fara teikna
+    const buffers = initBuffers(gl);
+
+    drawScene(gl, programInfo, buffers);
 }
 window.onload = main;
 
@@ -92,17 +96,17 @@ function loadShader(gl, type, source){
 
 function initBuffers(gl){
     //búa til buffer fyrir kassan sem ég er að fara búa til.
-    const positionBuffer = gl.creatBuffer();
+    const positionBuffer = gl.createBuffer();
 
     //sett þannig að positionBuffer er það sem bufferinn á að legjast á.
-    gl.blindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     //búa til array með púnktum þar sem kassinn á að ver á
     const positions = [
         -1.0,  1.0,
-        1.0,  1.0,
+         1.0,  1.0,
         -1.0, -1.0,
-        1.0, -1.0,
+         1.0, -1.0,
     ];
     //svo er hleypt þessu array í gegnum Float32Array og yfir á canvasið þar sem bufferinn er svo fylltur.
     gl.bufferData(gl.ARRAY_BUFFER,
@@ -116,7 +120,72 @@ function initBuffers(gl){
 function drawScene(gl, programInfo, buffers){
     gl.clearColor(0.0, 0.0, 0.0, 1.0,);     //gerir allt alveg svart
     gl.clearDepth(1.0);                     //hreinsar allt
-    gl.enable(gl.DEPTH_TEST);               //
-    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.DEPTH_TEST);               //enablar deapth testing
+    gl.depthFunc(gl.LEQUAL);                //hlutir sem eru nær eru teiknaðir yfir hluti sem eru fjær
+    
+    //hreinsar canvasinn áður en við byrjum að teikna á hann
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    //Búa til perspective matrix, sem er sérstök matrix fyrir myndavélina í senuni.
+    //fov-ið er 45 gráður sem segir hversu langt við getum séð í hverja átt.
+    const fieldOfView = 45 * Math.PI / 180 //fov er reiknað með radíunum
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;         //segir til um hvaða point er legst og styst frá myndavélinni
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(projectionMatrix,
+                     fieldOfView,
+                     aspect,
+                     zNear,
+                     zFar);
+
+    const modelViewMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix,         //destination.
+                   modelViewMatrix,         //matrix til þess að þýða.
+                   [-0.0, 0.0, -6.0]);      //valuið sem þarf að þýða í.
+
+    //þarf að segja hvernig WebGL á að taka út positionið og buffera því inn í vertexPosition attributið
+    {
+        const numComponents = 2;        // taka út 2 value í hvert skipti sem það er keyrt
+        const type = gl.FLOAT;          //gögnin eru í 32 bita float formi í buffernum
+        const normalize = false;        //ekki normaliza
+        const stride = 0;               //hversu marga bytes á að taka út úr valuenum
+                                        //ef það er núll þá á að nota type of numComponents að ofan
+        const offset = 0;               //á hvaða bita á að byrja inn í buffernum
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+
+        //basically að initializa með því að nota allt að ofan
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations,vertexPosition);
+    }
+
+    //segir webgl að nota programið þegar það er að teikna.
+    gl.useProgram(programInfo.program);
+
+    //setur shader uniform
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix
+    );
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix
+    );
+    {
+        const offset = 0;
+        const vertexCount = 4;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
 }
 
